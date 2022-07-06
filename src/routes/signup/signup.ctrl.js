c4d7440b529cfc36a47e6aab843c9cc66c042411
m1nsuppp <m1nsuppp@gmail.com>
2022-07-07 01:36:35 +0900

    inputUsername에 대한 유효성 체크 추가

--- src/routes/signup/signup.ctrl.js
+++ src/routes/signup/signup.ctrl.js
@@ -10,23 +10,35 @@ const showSignup = (req, res) => {
 
 const inputDataChecks = [
   check('inputEmail', 'Your email is not valid.').not().isEmpty(),
-  // check('username').not().isEmpty().isLength({ min: 2 }).withMessage('Username must have more than 4 characters.'),
+  check('inputUsername').not().isEmpty().isLength({ min: 2, max: 16 }).withMessage('Username must have more than 2 characters.'),
   check('inputPassword', 'Your password must be at least 8 characters.').not().isEmpty().isLength({ min : 8, max: 20}),
 ];
 
 const errorProcessing = (req, res) => {
-  const sql = `INSERT INTO accounts (email, password, username) VALUES (?);`;
+  const sql = `INSERT INTO accounts (email, username, password) VALUES (?);`;
   const errors = validationResult(req).errors;
   let errorMsgs = errors.map(error => error.msg);
+  let usernameLength = req.body.usernameLength;
+  let pwlen = req.body.pwlen;
+  let isSamePw = req.body.isSamePw;
+
+  let resData = {
+    usernameLength: usernameLength,
+    pwlen: pwlen,
+    isSamePw: isSamePw,
+  };
+
+  res.json(resData);
 
   if (!errorMsgs.length) {
-    let username = 'Rude Gulit';
     let inputEmail = req.body.inputEmail;
+    let inputUsername = req.body.inputUsername;
     let inputPassword = req.body.inputPassword;
     let confirmPassword = req.body.confirmPassword;
-    let accounts = [inputEmail, inputPassword, username];
+    let accounts = [];
 
     if (inputPassword === confirmPassword) {
+      accounts = [inputEmail, inputUsername, inputPassword];
       connection.query(sql, [accounts], (err, result) => {
         if (err) throw err;
       });
