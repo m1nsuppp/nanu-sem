6dc3715eef2901f17c139afa67a86e48ccb4e64e
m1nsuppp <m1nsuppp@gmail.com>
2022-07-07 03:01:28 +0900

    로그인되어 있으면 로그아웃 버튼으로 수정

---
+++ src/public/js/home.js
@@ -0,0 +1,19 @@
+"use strict";
+
+fetch('/', {
+  method: 'POST',
+  headers: {
+    'Access-Control-Origin': '*',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({})
+})
+.then((res) => res.json())
+.then((res) => {
+  if (res.isLoggedIn) {
+    document.querySelector('.login-icons').innerHTML = '로그아웃';
+    document.querySelector('.login-icons').addEventListener('click', () => {
+      window.location.href = '/logout';
+    })
+  }
+}).catch((error) => console.log(error));
