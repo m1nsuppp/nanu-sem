ea9295714307ec4a3f93657f2130e40904daad03
m1nsuppp <m1nsuppp@gmail.com>
2022-07-07 01:27:45 +0900

    비동기로 회원가입 유효성 체크

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,30 +1,65 @@
 "use strict";
 
+const inputUsername = document.getElementById('inputUsername');
 const inputPassword = document.getElementById('inputPassword');
-inputPassword.addEventListener('keyup', pwCheck);
+const confirmPassword = document.getElementById('confirmPassword');
 
+inputUsername.addEventListener('keyup', checkValidation);
+inputPassword.addEventListener('keyup', checkValidation);
+confirmPassword.addEventListener('keyup', checkValidation);
 
-function pwCheck() {
-  let pw = inputPassword.value.length;
+function isValid (resDataProp, validator) {
+  if (resDataProp) {
+    if ((validator.min <= resDataProp) && (resDataProp <= validator.max)) {
+      validator.selector.innerHTML = `유효합니다.`;
+    } else {
+      validator.selector.innerHTML = `${validator.min} ~ ${validator.max}자이어야 합니다.`;
+    }  
+  }
+}
 
-  fetch('http://localhost:3000/login', {
+function checkValidation () {
+  const lengthValidator = {
+    username: {
+      selector: document.querySelector('.validator.username'),
+      min: 2,
+      max: 16,
+    },
+    password: {
+      selector: document.querySelector('.validator.password'),
+      min: 8,
+      max: 20,
+    },
+  };
+  let usernameLength = inputUsername.value.length;
+  let ipw = inputPassword.value;
+  let cpw = confirmPassword.value;
+  let isSamePw = (ipw === cpw);
+
+  fetch('http://localhost:3000/signup', {
     method: 'POST',
     headers: {
       "Access-Control-Origin": "*",
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      "pwlen": pw,
+      "usernameLength": usernameLength,
+      "pwlen": ipw.length,
+      "isSamePw": isSamePw,
     }),
   }).then((res) => {
     return res.json();
   }).then((res) => {
-      console.log(res.pwlen);
-      if (res.pwlen < 8) {
-        document.getElementById('foo').innerHTML = 'your password must be at least 8 characters.';
+    isValid(res.usernameLength, lengthValidator.username);
+    isValid(res.pwlen, lengthValidator.password);
+    
+    if (cpw) {
+      if (isSamePw) {
+        document.querySelector('.validator.confirm').innerHTML = `비밀번호가 일치합니다.`;
       } else {
-        document.getElementById('foo').innerHTML = 'possible';
+        document.querySelector('.validator.confirm').innerHTML = `비밀번호가 일치하지 않습니다.`;
       }
+    }
   }).catch((error) => {
     console.log(error);
   });
