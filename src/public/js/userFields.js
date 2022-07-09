3034a9a68713520ac50d7616ccb44d455835f340
m1nsuppp <m1nsuppp@gmail.com>
2022-07-09 22:33:18 +0900

    회원가입 form fields 선언부

---
+++ src/public/js/userFields.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const inputEmail = document.getElementById('inputEmail');
+const inputUsername = document.getElementById('inputUsername');
+const inputPassword = document.getElementById('inputPassword');
+const confirmPassword = document.getElementById('confirmPassword');
+const colors = {
+  correct: `#03c75a`,
+  incorrect: `#ff003e`,
+};
+
+export { inputEmail, inputUsername, inputPassword, confirmPassword, colors };
