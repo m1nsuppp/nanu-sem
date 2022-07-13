6aa6e25431d1fed562a73df0285b3557241ce8f6
m1nsuppp <m1nsuppp@gmail.com>
2022-07-14 01:34:24 +0900

    await으로 리팩토링

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,18 +1,17 @@
 "use strict";
 
-fetch('/', {
-  method: 'POST',
-  headers: {
-    'Access-Control-Origin': '*',
-    'Content-Type': 'application/json',
-  },
-})
-.then((res) => res.json())
-.then((res) => {
-  if (res.isLoggedIn) {
-    document.querySelector('.login-icons').innerHTML = '로그아웃';
-    document.querySelector('.login-icons').addEventListener('click', () => {
-      window.location.href = '/logout';
-    });
-  }
-}).catch((error) => console.log(error));
+async function home() {
+  const url = '/';
+  let response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Access-Control-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+  })
+  .catch((error) => console.log(error));
+  let result = await response.json().catch((error) => console.log(error));
+  console.log(result);
+}
+
+home();
