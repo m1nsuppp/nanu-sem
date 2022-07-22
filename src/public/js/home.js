a57e437f84d9bac5bd3988e2ae3ea1529d6d96d3
m1nsuppp <m1nsuppp@gmail.com>
2022-07-23 02:06:29 +0900

    url이 포함된 변수명 대문자로 변경

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -8,11 +8,11 @@ async function isSignedIn() {
   const linkSignIn = document.querySelector('#link-signin');
   const linkCreate = document.querySelector('#link-create');
   const signStates = ['로그인', '로그아웃'];
-  const signUrls = ['/signin', '/signout'];
-  const createUrls = ['/signin', '/create'];
-  const url = '/';
+  const signURLs = ['/signin', '/signout'];
+  const createURLs = ['/signin', '/create'];
+  const URL = '/';
  
-  let response = await fetch(url, {
+  let response = await fetch(URL, {
     method: 'POST',
   })
   .catch(error => console.log(error));
@@ -20,8 +20,8 @@ async function isSignedIn() {
   let result = await response.json().catch(error => console.log(error));
   
   linkSignIn.innerHTML = signStates[Number(result.isSignedIn)];
-  linkSignIn.href = signUrls[Number(result.isSignedIn)];
-  linkCreate.href = createUrls[Number(result.isSignedIn)];
+  linkSignIn.href = signURLs[Number(result.isSignedIn)];
+  linkCreate.href = createURLs[Number(result.isSignedIn)];
 }
 
 home();
